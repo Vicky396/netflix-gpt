00817ac10a8c323784933ce971221c409db02947
Vikash Singh <vikashsingh396@gmail.com>
2025-07-20 03:11:28 +0530

    Movies List creation

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import MovieList from './MovieList'
+import { useSelector } from 'react-redux'
 
 const SecondaryContainer = () => {
+  const movies=useSelector(store=>store.movies);
   return (
-    <div>SecondaryContainer</div>
+    movies &&(
+    <div className='bg-black'>
+      <div className='relative z-20 -mt-20'>
+      <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies}/>
+      <MovieList title={"Upcoming"} movies={movies?.upcomingMovies}/>
+      <MovieList title={"Popular"} movies={movies?.popularMovies}/>
+      <MovieList title={"Top Rated"} movies={movies?.topRatedMovies}/>
+    </div>
+    </div>
+    )
   )
 }
 
-export default SecondaryContainer
+export default SecondaryContainer;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ export const USER_AVATAR =
   "https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg";
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/8200f588-2e93-4c95-8eab-ebba17821657/web/IN-en-20250616-TRIFECTA-perspective_9cbc87b2-d9bb-4fa8-9f8f-a4fe8fc72545_large.jpg";
+export const MOVIE_IMG="https://image.tmdb.org/t/p/w500/";
 export const API_OPTIONS = {
   method: "GET",
   headers: {
